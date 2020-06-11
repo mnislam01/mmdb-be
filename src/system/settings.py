a3dc9b5fa05b06b7e6cae8856783a15ad26ef0fb
mnislam01 <mnazrul.c@gmail.com>
2020-06-11 13:47:54 +0600

    management command to wait for db

--- src/system/settings.py
+++ src/system/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< HEAD
     'rest_framework',
     'rest_framework.authtoken',
     'system',
     'base',
     'movie',
+=======
+    'system',
+>>>>>>> management command to wait for db
 ]
 
 MIDDLEWARE = [
